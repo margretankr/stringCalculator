9956c825b0c1fb30e1ba5a9d786a97363a17c246
margretankr <margretannakristjansdottir@Margrets-MacBook-Pro.local>
2015-10-17 16:32:54 +0000

    Create a String calculator with a method int Add(string numbers)

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -3,11 +3,39 @@ package is.ru.stringcalculator;
 public class Calculator {
 
 	public static int add(String text){
-	
-		return 1;
+		if (text.equals(""))
+		
+			return 0;
+		
+		else if (text.contains(","))
+		{	
+			return sum(splitNumbers(text));
+		}
+		
+		else 
+			return 1;		
 	}
 
+	private static String [] splitNumbers (String numbers)
+	{
+		
 
+		return numbers.split(",");
+	}
 
+	private static int toInt(String number){
+
+		return Integer.parseInt(number);
+	}
+
+	private static int sum(String[] numbers){
+ 
+
+ 	    int total = 0;
+        for(String number : numbers){
+		    total += toInt(number);
+		}
+		return total;
+    }
 
 }
